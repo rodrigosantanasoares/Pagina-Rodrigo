4aab9afb3b39de648a1278fe5f2b42a5367d69f2
Rodrigo Santana <rodrigo.santana.1@hotmail.com>
2026-01-22 15:17:37 -0300

    versao 1

--- js/main.js
+++ js/main.js
@@ -2,3 +2,21 @@ AOS.init({
   duration: 1000,
   once: true,
 });
+
+/* Modal da galeria */
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modal-img');
+
+if (modal && modalImg) {
+  document.querySelectorAll('.gallery-img').forEach(img => {
+    img.addEventListener('click', () => {
+      modal.classList.remove('hidden');
+      modalImg.src = img.src;
+    });
+  });
+
+  modal.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    modalImg.src = '';
+  });
+}
